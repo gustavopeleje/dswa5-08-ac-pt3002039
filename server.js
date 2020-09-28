89363220105ab92e8539f5d207a2b12bc6e48dea
Jader Thomé <jaderthome2@gmail.com>
2020-09-28 20:14:28 -0300

    Atualiza

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var http = require('http');
 var app = require('./config/express')();
+const url =
+'mongodb+srv://DSWA5:aretuza123@cluster0.cjyyi.mongodb.net/ifsp?retryWrites=true&w=majority';
+require('./config/database.js')(url);
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express Server escutando na porta ' + app.get('port'));
 });
+
+
