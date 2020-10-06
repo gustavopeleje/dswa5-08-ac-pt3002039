b0a79b0246e9bffdbbf1cd648c1d0ec339c7fcdb
Jader Thomé <jaderthome2@gmail.com>
2020-10-06 10:31:47 -0300

    Atualiza

--- app/models/contato.js
+++ app/models/contato.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 module.exports = function() {
     var schema = mongoose.Schema({
         nome: {type: String, required: true},
-        email: {type: String, required: true, index: { unique: not null} }
+        email: {type: String, required: true, index: { unique: null} }
     });
     return mongoose.model('Contato', schema);
 };
