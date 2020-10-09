ab6662a8dee1c9a282c2ada56db406227c3c20b8
Jader Thomé <jaderthome2@gmail.com>
2020-10-09 18:17:29 -0300

    Atualiza

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 module.exports = function(uri) {
-    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: false });
+    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
     mongoose.connection.on('connected', function() {
         console.log('Mongoose! Conectado em ' + uri);
